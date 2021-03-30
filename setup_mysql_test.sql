9edb4d0af2f4134e982f12b45c6b3bdb8c0046d1
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-03-30 18:10:06 +0000

    setup test

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -8,14 +8,14 @@
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER
-    IF NOT EXISTS 'hbnb_dev'@'localhost'
-    IDENTIFIED BY 'hbnb_dev_pwd';
+    IF NOT EXISTS 'hbnb_test_db'@'localhost'
+    IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES
    ON `hbnb_dev_db`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 GRANT SELECT
    ON `performance_schema`.*
-   TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
+   TO 'hbnb_test'@'localhost'
+   IDENTIFIED BY 'hbnb_test_pwd';
 FLUSH PRIVILEGES;
