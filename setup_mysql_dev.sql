3d487a985f218973c6248528fa0fa133bbdbea24
Wendy-Omondi <w.omondi19@launchpad.alx.app>
2021-03-31 13:50:02 +0000

    fix grants ID

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -13,9 +13,7 @@ CREATE USER
 GRANT ALL PRIVILEGES
    ON `hbnb_dev_db`.*
    TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT SELECT
    ON `performance_schema`.*
    TO 'hbnb_dev'@'localhost'
-   IDENTIFIED BY 'hbnb_dev_pwd';
 FLUSH PRIVILEGES;
